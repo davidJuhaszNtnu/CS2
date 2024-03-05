c40fb745c92891af66fe1017d7922c885b6690a4
davidJuhaszNtnu <115979856+davidJuhaszNtnu@users.noreply.github.com>
2024-03-05 15:12:15 +0100

    05.03.2024

--- Waterkennis/Assets/Scripts/PanelLayoutHandler.cs
+++ Waterkennis/Assets/Scripts/PanelLayoutHandler.cs
@@ -10,7 +10,7 @@ public class PanelLayoutHandler : MonoBehaviour
     void Start()
     {
         restart();
-        // panels[8].SetActive(true);
+        // panels[40].SetActive(true);
     }
 
     public void restart(){
